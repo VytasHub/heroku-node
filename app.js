ce387a83bbe94798592062b5d6e83bd91845801d
VytasHub <vytasvaiciulisgti@gmail.com>
2016-01-24 21:55:47 +0000

    Revert "LogIn"
    This reverts commit 5941972e27fa0636d074d65ea1eb3e7ee54c1327.

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-// app.js
-angular.module('YOUR-APP-NAME', ['auth0', 'angular-storage', 'angular-jwt'])
-.config(function (authProvider) {
-  authProvider.init({
-    domain: 'book.eu.auth0.com',
-    clientID: 'yFtHBRZ0ZNdFaWUKSIr3CJzsqR4KuqgY'
+var lock = new Auth0Lock('YOUR_CLIENT_ID', 'YOUR_DOMAIN');
+
+
+document.getElementById('btn-login').addEventListener('click', function() {
+  lock.show(function(err, profile, token) {
+    if (err) {
+      // Error callback
+      console.error("Something went wrong: ", err);
+    } else {
+      // Success calback  
+
+      // Save the JWT token.
+      localStorage.setItem('userToken', token);
+      // Save the profile
+      localStorage.setItem('userProfile', JSON.stringify(profile));
+    }
   });
-})
-.run(function(auth) {
-  // This hooks al auth events to check everything as soon as the app starts
-  auth.hookEvents();
 });
