5941972e27fa0636d074d65ea1eb3e7ee54c1327
VytasHub <vytasvaiciulisgti@gmail.com>
2016-01-24 21:41:54 +0000

    LogIn
    LogIn

---
+++ LoginCtrl.js
@@ -0,0 +1,14 @@
+// LoginCtrl.js
+angular.module('YOUR-APP-NAME').controller('LoginCtrl', ['$scope', '$http', 'auth', 'store', '$location',
+function ($scope, $http, auth, store, $location) {
+  $scope.login = function () {
+    auth.signin({}, function (profile, token) {
+      // Success callback
+      store.set('profile', profile);
+      store.set('token', token);
+      $location.path('/');
+    }, function () {
+      // Error callback
+    });
+  }
+}]);
